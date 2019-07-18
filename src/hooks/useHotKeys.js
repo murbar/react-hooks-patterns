f6d6b26c6a8d10d8f88aa58672ba8601936a40c9
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-07-18 12:09:03 -0400

    Add hooks

---
+++ src/hooks/useHotKeys.js
@@ -0,0 +1,40 @@
+import { useEffect, useRef } from 'react';
+
+// keyHandlerMap = {
+//   key: callback,
+//   eg.
+//   'r': reset
+// }
+
+export default function useHotKeys(keyHandlerMap) {
+  // TODO verify map is string and functions
+
+  const targets = Object.keys(keyHandlerMap);
+  const keydown = useRef(false);
+
+  useEffect(() => {
+    const downHandler = ({ key }) => {
+      // check for long press
+      if (keydown.current) return;
+
+      if (targets.includes(key)) {
+        keydown.current = true;
+        const callback = keyHandlerMap[key];
+        callback();
+      }
+    };
+
+    const upHandler = ({ key }) => {
+      if (targets.includes(key)) {
+        keydown.current = false;
+      }
+    };
+
+    window.addEventListener('keydown', downHandler);
+    window.addEventListener('keyup', upHandler);
+    return () => {
+      window.removeEventListener('keydown', downHandler);
+      window.removeEventListener('keyup', upHandler);
+    };
+  }, [targets, keyHandlerMap]);
+}
---
+++ src/hooks/useKeyboardQuery.js
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+
+// add a class to body when user is using keyboard
+
+export default function useKeyboardQuery(className) {
+  useEffect(() => {
+    const keyboardInUse = () => document.body.classList.add(className);
+    const mouseInUse = () => document.body.classList.remove(className);
+
+    document.body.addEventListener('keydown', keyboardInUse);
+    document.body.addEventListener('mousedown', mouseInUse);
+    return () => {
+      document.body.removeEventListener('keydown', keyboardInUse);
+      document.body.removeEventListener('mousedown', mouseInUse);
+    };
+  }, [className]);
+}
