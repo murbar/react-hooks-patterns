6dc7487ff9e468a1d3b8ea43a46d8078b9ebe26d
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-08-11 11:08:23 -0400

    Remove app

---
+++ src/appStateContext.js
@@ -0,0 +1,21 @@
+import React, { createContext, useState, useMemo, useContext } from 'react';
+
+const AppStateContext = createContext();
+
+const AppStateProvider = ({ children }) => {
+  const [state, setState] = useState({});
+
+  const contextValue = useMemo(() => {
+    const someAction = () => {
+      setState(prev => ({ ...prev, thing: 'done' }));
+    };
+
+    return { state, someAction };
+  }, [state]);
+
+  return <AppStateContext.Provider value={contextValue}>{children}</AppStateContext.Provider>;
+};
+
+const useAppState = () => useContext(AppStateContext);
+
+export { AppStateProvider, useAppState };
