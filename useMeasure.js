19251d25526f373e68045a0d93ec186e4858082d
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-08-15 10:56:15 -0400

    Update from old hooks syntax

--- useMeasure.js
+++ useMeasure.js
@@ -5,7 +5,12 @@ export default function useMeasure() {
   const ref = useRef();
   const [bounds, set] = useState({ left: 0, top: 0, width: 0, height: 0 });
   const [ro] = useState(() => new ResizeObserver(([entry]) => set(entry.contentRect)));
-  useEffect(() => (ro.observe(ref.current), ro.disconnect), []);
+
+  useEffect(() => {
+    ro.observe(ref.current);
+    return () => ro.disconnect();
+  }, [ro]);
+
   return [{ ref }, bounds];
 }
 
