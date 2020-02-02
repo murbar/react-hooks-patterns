c832d9ca01fd70af39d1b87fb88444882b01c8c9
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2020-02-02 12:14:26 -0500

    Rename hook

--- useDebugLogging.js
+++ useDebugLogging.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 
 const envIsDevelopment = process.env.NODE_ENV === 'development';
 
-export default function useLogging(data, label = 'Data') {
+export default function useDebugLogging(data, label = 'Data') {
   useEffect(() => {
     if (envIsDevelopment) console.log(label, data);
   }, [data, label]);
